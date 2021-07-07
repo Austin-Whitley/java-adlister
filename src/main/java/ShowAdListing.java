990f4f8267730463a5c1d422aeeee8261703f1dc
Austin Whitley <whitley.austin01@gmail.com>
2021-07-07 11:24:54 -0500

    added a few lines to the showadlisting

--- src/main/java/ShowAdListing.java
+++ src/main/java/ShowAdListing.java
@@ -12,6 +12,8 @@ public class ShowAdListing extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
         // create a new instance of the ListAdsDao class so we can use the all() method
         Ads adsDao = new ListAdsDao();
+        //Ads adsDao = DaoFactory.getAdsDao(); is a better usage of dao
+
         //set a new List of <ad objects> equal to our list of ads (.all() method)
         List<Ad> ads = adsDao.all();
         // Pass the data to the jsp
